03b576afe1e5c9295aa8942e19f53d088a39f7c7
TDGLU <tdglukealt@gmail.com>
2023-03-09 18:21:39 -0800

    adding pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+// Styles 
+import "../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+
+  return (
+
+  )
+}
+
+export default Footer;
--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,3 +1,40 @@
 import React from 'react';
 
+import About from './components/pages/About'
+import Home from './components/pages/Home'
+import Contant from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Work from './components/Work'
+
+const PortfolioContainer = () => {
+
+  const location = useLocation();
+
+  return (
+    <>
+      <Navbar />
+
+      {/* Page  */}
+      <div className="page-wrapper">
+       <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/projects" element={<Work />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+
+      <Footer  />
+    </>
+  )
+}
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+
 export default PortfolioContainer;
